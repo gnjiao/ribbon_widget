4d6812c88261a9ca60a8877ce465a8395ab2b400
xianbinsu <656923369@qq.com>
2019-09-01 11:21:34 +0800

    2019/9/1

---
+++ MenuItemWidget.cpp
@@ -0,0 +1,1 @@
+#include "MenuItemWidget.h"
---
+++ MenuItemWidget.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <QWidget>
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QStyleOption>
+#include <QPaintEvent>
+#include <QPixmap>
+#include <QPainter>
+class MenuItemWidget : public QWidget
+{
+	Q_OBJECT
+
+public:
+	MenuItemWidget(const QPixmap& icon, const QString& text, QWidget *parent = 0)
+	{
+		QLabel* label_Icon = new QLabel(this);
+		label_Icon->setFixedSize(32, 32);
+		label_Icon->setScaledContents(true);
+		label_Icon->setPixmap(icon);
+
+		QLabel* label_Text = new QLabel(text, this);
+
+		QHBoxLayout* layout = new QHBoxLayout;
+		layout->setContentsMargins(20, 6, 50, 6);
+		layout->setSpacing(10);
+		layout->addWidget(label_Icon);
+		layout->addWidget(label_Text);
+		setLayout(layout);
+
+		setFixedWidth(240);
+	}
+	~MenuItemWidget()
+	{}
+
+protected:
+	void paintEvent(QPaintEvent* e) Q_DECL_OVERRIDE {
+		QStyleOption opt;
+		opt.init(this);
+		QPainter p(this);
+		style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
+	}
+
+private:
+};
+
